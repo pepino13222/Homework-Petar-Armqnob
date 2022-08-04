abca8250e0024c16cf483e02cde40e7c82c6318f
pepino13222 <pepino1322@gmail.com>
2022-08-04 23:19:56 +0300

    added the operations and the reading from the files

--- zad_armqnvov/zad_armqnvov.cpp
+++ zad_armqnvov/zad_armqnvov.cpp
@@ -35,6 +35,9 @@ bool predicateTwo(const int& element, Vector<int>& args) {
 int main()
 {
     Vector <int> testVector;
+    Vector<int> finalVector;
+
+    Multitude* currentMultitude;
 
     testVector.add(1);
     testVector.add(2);
@@ -64,16 +67,80 @@ int main()
    // testPred->getCollection().print();
     //printf("%d %d \n", testVector[0], testVector[1]);0
 
-    /*std::ifstream is;
+    std::ifstream is;
     is.open("Input.txt");
     if (is.is_open()) {
         while (is) {
             char buffer[256];
             is.getline(buffer, 255);
             std::ifstream isb;
-            isb.open(buffer);
+            isb.open(buffer, std::ios::out | std::ios::binary);
+            uint16_t N, T;
+            if (isb.is_open()) {
+                isb.read((char*)&N, sizeof(N));
+                isb.read((char*)&T, sizeof(T));
+                int n = (int)N;
+                int t = (int)T;
+                Vector<int> tempVector;
+                for (size_t i = 0; i < n; i++)
+                {
+                    uint32_t readed;
+                    isb.read((char*)&readed, sizeof(readed));
+                    tempVector.add((int)readed);
+                }
+                if (t == 0) {
+                    Multitude* temp = new DeadMult(tempVector);
+                    Vector<Multitude*> collectionMultitudes;
+                    collectionMultitudes.add(temp);
+                    Multitude* temp2 = new SecondFunctionCLass(finalVector, collectionMultitudes);
+                    finalVector = temp2->getCollection();
+                }
+                else if (t == 1) {
+                    Multitude* temp = new PreedicateMulti(predicateTwo, finalVector, tempVector);
+                    Vector<Multitude*> collectionMultitudes;
+                    collectionMultitudes.add(temp);
+                    Multitude* temp2 = new SecondFunctionCLass(finalVector, collectionMultitudes);
+                    finalVector = temp2->getCollection();
+                }
+                else if (t == 2) {
+                    Multitude* temp = new PreedicateMulti(predicateOne, finalVector, tempVector);
+                    Vector<Multitude*> collectionMultitudes;
+                    collectionMultitudes.add(temp);
+                    Multitude* temp2 = new SecondFunctionCLass(finalVector, collectionMultitudes);
+                    finalVector = temp2->getCollection();
+                }
+                else if (t == 8) {
+                    Multitude* temp = new DeadMult(tempVector); Vector<Multitude*> collectionMultitudes;
+                    collectionMultitudes.add(temp);
+                    Multitude* temp2 = new FirstFunctionClass(finalVector, collectionMultitudes);
+                    finalVector = temp2->getCollection();
+                }
+                else if (t == 9) {
+                    Multitude* temp = new PreedicateMulti(predicateTwo, finalVector, tempVector);
+                    Vector<Multitude*> collectionMultitudes;
+                    collectionMultitudes.add(temp);
+                    Multitude* temp2 = new FirstFunctionClass(finalVector, collectionMultitudes);
+                    finalVector = temp2->getCollection();
+                }
+                else if (t == 10) {
+                    Multitude* temp = new PreedicateMulti(predicateOne, finalVector, tempVector);
+                    Vector<Multitude*> collectionMultitudes;
+                    collectionMultitudes.add(temp);
+                    Multitude* temp2 = new FirstFunctionClass(finalVector, collectionMultitudes);
+                    finalVector = temp2->getCollection();
+                }
+            }
         }
-    }*/
+    }
+
+    int first = 0, second = 0;
+
+    std::cin >> first >> second;
+
+    for (size_t i = 0; i < finalVector.getSize(); i++)
+    {
+        if (finalVector[i] > first && finalVector[i] < second)printf("%d", finalVector[i]);
+    }
 
     return 0;
 }
